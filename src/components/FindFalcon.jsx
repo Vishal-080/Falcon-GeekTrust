b423b92fc25c02966737b7301b0f1fc2becd7c62
Vishal Rathod <vrathod591@gmail.com>
2025-08-11 01:48:22 +0530

    Find Falcon pages created

--- src/components/FindFalcon.jsx
+++ src/components/FindFalcon.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React from "react";
 
 const FindFalcon = () => {
   return (
-    <div>FindFalcon</div>
-  )
-}
+    <div>
+      <h1>Finding Falcone!</h1>
+      <option>
+        <select name="" id="">
+          Planet 1
+        </select>
+        <select name="" id="">
+          Planet 2
+        </select>
+        <select name="" id="">
+          Planet 3
+        </select>
+        <select name="" id="">
+          Planet 4
+        </select>
+        <select name="" id="">
+          Planet 5
+        </select>
+      </option>
+    </div>
+  );
+};
 
 export default FindFalcon;
