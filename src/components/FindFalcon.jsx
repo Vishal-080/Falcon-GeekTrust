c46070123fbfea12ac111f124909e852474b34b3
Vishal Rathod <vrathod591@gmail.com>
2025-08-16 01:44:54 +0530

    find falcon coding

--- src/components/FindFalcon.jsx
+++ src/components/FindFalcon.jsx
@@ -5,72 +5,32 @@ const FindFalcon = () => {
     <div>
       <h1>Finding Falcone!</h1>
       <select>
-        <option value="planet 1">
-          Planet 1
-        </option>
-        <option value="planet 2">
-          Planet 2
-        </option>
-        <option value="planet 3">
-          Planet 3
-        </option>
-        <option value="planet 4">
-          Planet 4
-        </option>
-        <option value="planet 5">
-          Planet 5
-        </option>
+        <option value="planet 1">Planet 1</option>
+        <option value="planet 2">Planet 2</option>
+        <option value="planet 3">Planet 3</option>
+        <option value="planet 4">Planet 4</option>
+        <option value="planet 5">Planet 5</option>
       </select>
       <select>
-        <option value="planet 1">
-          Planet 1
-        </option>
-        <option value="planet 2">
-          Planet 2
-        </option>
-        <option value="planet 3">
-          Planet 3
-        </option>
-        <option value="planet 4">
-          Planet 4
-        </option>
-        <option value="planet 5">
-          Planet 5
-        </option>
+        <option value="planet 1">Planet 1</option>
+        <option value="planet 2">Planet 2</option>
+        <option value="planet 3">Planet 3</option>
+        <option value="planet 4">Planet 4</option>
+        <option value="planet 5">Planet 5</option>
       </select>
       <select>
-        <option value="planet 1">
-          Planet 1
-        </option>
-        <option value="planet 2">
-          Planet 2
-        </option>
-        <option value="planet 3">
-          Planet 3
-        </option>
-        <option value="planet 4">
-          Planet 4
-        </option>
-        <option value="planet 5">
-          Planet 5
-        </option>
+        <option value="planet 1">Planet 1</option>
+        <option value="planet 2">Planet 2</option>
+        <option value="planet 3">Planet 3</option>
+        <option value="planet 4">Planet 4</option>
+        <option value="planet 5">Planet 5</option>
       </select>
       <select>
-        <option value="planet 1">
-          Planet 1
-        </option>
-        <option value="planet 2">
-          Planet 2
-        </option>
-        <option value="planet 3">
-          Planet 3
-        </option>
-        <option value="planet 4">
-          Planet 4
-        </option>
-        <option value="planet 5">
-          Planet 5
-        </option>
+        <option value="planet 1">Planet 1</option>
+        <option value="planet 2">Planet 2</option>
+        <option value="planet 3">Planet 3</option>
+        <option value="planet 4">Planet 4</option>
+        <option value="planet 5">Planet 5</option>
       </select>
       <button>Search</button>
     </div>
